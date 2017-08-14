fb3c9b2e0c9e96a72b2fb787df38ea2c00cc7f72
Mathias Sejrup Plougmann <mathias_j93@hotmail.com>
2017-08-14 12:39:25 +0200

    Initial commit + 9 first exercises

---
+++ CrashCourse/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CrashCourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+
+            //Console.WriteLine("Hello Mathias!");
+
+            //2
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to add:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //var Sum = FirstNumber + SecondNumber;
+            //Console.WriteLine($"{FirstNumber} + {SecondNumber} = {Sum}");
+
+            //3
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to divide:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //var Sum = FirstNumber / SecondNumber;
+            //Console.WriteLine($"{FirstNumber} / {SecondNumber} = {Sum}");
+
+            //4
+
+            //var Sum1 = -1 + 4 * 6;
+            //var Sum2 = (35 + 5) % 7;
+            //var Sum3 = 14 + -4 * 6 / 11;
+            //var Sum4 = 2 + 15 / 6 * 1 - 7 % 2;
+            //Console.WriteLine($"{Sum1} \n{Sum2} \n{Sum3} \n{Sum4}");
+
+            //5
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to swap:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //var StoredNumber = FirstNumber;
+            //FirstNumber = SecondNumber;
+            //SecondNumber = StoredNumber;
+            //Console.WriteLine($"First Number : {FirstNumber} \nSecond Number: {SecondNumber}");
+
+            //6
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type a number to multiply:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type another number to multiply");
+            //var ThirdNumber = int.Parse(Console.ReadLine());
+
+            //var Sum = FirstNumber * SecondNumber * ThirdNumber;
+            //Console.WriteLine($"{FirstNumber} * {SecondNumber} * {ThirdNumber} = {Sum}");
+
+            //7
+
+            //Console.WriteLine("Type a number:");
+            //var FirstNumber = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Type another number:");
+            //var SecondNumber = int.Parse(Console.ReadLine());
+
+            //var ResultPlus = FirstNumber + SecondNumber;
+            //var ResultMinus = FirstNumber - SecondNumber;
+            //var ResultMultiply = FirstNumber * SecondNumber;
+            //var ResultDivide = FirstNumber / SecondNumber;
+            //var ResultMod = FirstNumber % SecondNumber;
+
+            //Console.WriteLine($"{FirstNumber} + {SecondNumber} = {ResultPlus}" +
+            //                  $"\n{FirstNumber} - {SecondNumber} = {ResultMinus}" +
+            //                  $"\n{FirstNumber} x {SecondNumber} = {ResultMultiply}" +
+            //                  $"\n{FirstNumber} / {SecondNumber} = {ResultDivide}" +
+            //                  $"\n{FirstNumber} mod {SecondNumber} = {ResultMod}");
+
+            //8
+
+            //Console.WriteLine("Type a number:");
+            //var number = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i <= 10; i++)
+            //{
+            //    var Sum = number * i;
+            //    Console.WriteLine(Sum);
+            //}
+
+            //9
+
+            //Console.WriteLine("Enter the first number:");
+            //var firstNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the second number:");
+            //var secondNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the third number:");
+            //var thirdNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter the forth number:");
+            //var forthNumber = int.Parse(Console.ReadLine());
+
+            //var numberList = new List<int>();
+
+            //numberList.Add(firstNumber);
+            //numberList.Add(secondNumber);
+            //numberList.Add(thirdNumber);
+            //numberList.Add(forthNumber);
+
+            //var avg = 0;
+
+            //foreach (var number in numberList)
+            //{
+            //    avg += number;
+            //}
+
+            //avg /= numberList.Capacity;
+
+            //Console.WriteLine($"The average of {firstNumber}, {secondNumber}, {thirdNumber}, {forthNumber} is: {avg}");
+
+            //10
+
+
+            //Shutdown
+            Console.ReadLine();
+        }
+    }
+}
